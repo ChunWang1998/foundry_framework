ebf1eef5e6b8f462ac74f998ac6cc08c5b8dd4a6
leo-chia1105 <126639205+leo-chia1105@users.noreply.github.com>
2023-10-19 15:40:46 +0800

    update

--- practice1/test/test1.sol
+++ practice1/test/test1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Depositor} from "../src/Depositor.sol";
 import {Receiver} from "../src/Receiver.sol";
 import "forge-std/console.sol";
